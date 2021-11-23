945bb7c82e3aac88ccc7417a8e603a55790b952c
yuichi2670 <yuichitao2670@gmail.com>
2021-11-23 20:34:37 +0900

    headerPC

---
+++ marine-associate/front-page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="l-main-visual p-main-visual">
+    <div class="p-main-visual__inner l-inner">
+      <div class="p-main-visual__container">
+        <div class="p-main-visual__picture">
+          <img src="<?php echo get_template_directory_uri() ?>./assets/img/main-visual.jpg" alt="">
+        </div><!-- /p-main-visual__picture -->
+        <div class="p-main-visual__comments">
+          <p class="p-main-visual__japanese">『一生のうちで必ず叶えたいこと』の実現へ。</p><!-- /p-main-visual__japanese -->
+          <p class="p-main-visual__japanese">私達が全力でサポートします。</p><!-- /p-main-visual__japanese -->
+          <p class="p-main-visual__english">Toward the realization of "what I want to achieve in my lifetime” , We will do our best to support you.</p><!-- /p-main-visual__english -->
+        </div><!-- /p-main-visual__texts -->
+      </div><!-- /p-main-visual__container -->
+    </div><!-- /p-main-visual__inner -->
+  </div><!-- /p-main -->
+
+<?php get_footer(); ?>
---
+++ marine-associate/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap" rel="stylesheet">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <header class="l-header p-header">
+    <div class="p-header__inner">
+      <div class="p-header__title"><span>MARINE</span><span>ACTIVITY</span><span>ASSOCIATION</span><span>一般社団法人 渋川マリンアクティビティ協会</span></div><!-- /p-header__title -->
+      <div class="p-header__nav">
+        <ul class="p-header__nav-links">
+          <li class="c-header__nav-link"><a href="">ABOUT<span>私たちの想い</span></a></li>
+          <li class="c-header__nav-link"><a href="">PROJECT<span>実施事業</span></a></li>
+          <li class="c-header__nav-link"><a href="">SPONSER<span>スポンサー</span></a></li>
+          <li class="c-header__nav-link"><a href="">NEWS<span>新着情報</span></a></li>
+          <li class="c-header__nav-link"><a href="">ACCESS<span>アクセス</span></a></li>
+        </ul>
+        <div class="p-header__contact"><a href="">CONTACT<span>お問い合わせ</span></a></div><!-- /p-header__contact -->
+      </div><!-- /p-header__nav -->
+    </div><!-- /p-header__inner -->
+  </header>
+
+  
