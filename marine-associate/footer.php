d8bef8912e6eef0a79c680076c9fafa5021354a8
yuichi2670 <yuichitao2670@gmail.com>
2021-11-24 14:59:53 +0900

    フッターPC

--- marine-associate/footer.php
+++ marine-associate/footer.php
@@ -1,4 +1,52 @@
-<footer class="">
+<div class="l-top p-top">
+    <a href="#" class="p-top__items">
+        <div class="c-top__arrow"></div><!-- /c-top__arrow -->
+        <div class="c-top__text">PAGE TOP</div>
+    </a>
+</div><!-- /p-top -->
+<footer class="l-footer p-footer">
+    <div class="p-footer__inner l-inner">
+        <div class="p-footer__container">
+            <div class="p-footer__items">
+                <div class="p-footer__title"><span>MARINE</span><span>ACTIVITY</span><span>ASSOCIATION</span><span>一般社団法人 渋川マリンアクティビティ協会</span></div><!-- /p-footer__title -->
+                <div class="p-footer__access">
+                    <div class="c-footer__address">〒706-0024  岡山県玉野市御崎2-13-21</div><!-- /p-footer__address -->
+                    <div class="c-footer__tel">TEL    ： 050-3503-1079</div><!-- /c-footer__tel -->
+                    <div class="c-footer__mail">MAIL   ： info@you-make-okayama.jp</div><!-- /c-footer__mail -->
+                </div><!-- /p-footer__access -->
+                <ul class="p-footer__sns">
+                    <li class="p-footer__sns-item">
+                        <a href="#" class="c-footer__sns-item-link" aria-label="instagram"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="p-footer__sns-item">
+                        <a href="#" class="c-footer__sns-item-link" aria-label="twitter"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="p-footer__sns-item">
+                        <a href="#" class="c-footer__sns-item-link" aria-label="facebook"><i class="fab fa-facebook-f"></i></i></a>
+                    </li>
+                </ul><!-- /p-footer__sns -->
+            </div><!-- /footer__items -->
+            <div class="p-footer__nav">
+                <ul class="p-footer__nav-links">
+                    <li class="c-footer__nav-link"><a href="">ABOUT<span>私たちの想い</span></a></li>
+                    <li class="c-footer__nav-link"><a href="">PROJECT<span>実施事業</span></a></li>
+                    <li class="c-footer__nav-link"><a href="">SPONSER<span>スポンサー</span></a></li>
+                    <li class="c-footer__nav-link"><a href="">NEWS<span>新着情報</span></a></li>
+                    <li class="c-footer__nav-link"><a href="">ACCESS<span>アクセス</span></a></li>
+                </ul>
+          </div><!-- /p-header__nav -->
+        </div><!-- /p-footer__container -->
+        <div class="p-footer__contents">
+            <div class="p-footer__otherLinks">
+                <a href="" class="c-footer__otherLink">会社情報</a>
+                <a href="" class="c-footer__otherLink">プライバシーポリシー</a>
+                <a href="" class="c-footer__otherLink">サイトマップ</a>
+            </div><!-- /p-footer__otherLinks -->
+            <div class="p-footer__copy">
+                &copy; 2021 MARINE ACTIVITY ASSOCIATION .
+            </div><!-- /p-footer__copy -->
+        </div><!-- /p-footer__contents -->
+    </div><!-- /footer__inner -->
 </footer>
 <?php wp_footer(); ?>
 </body>
