65acecd07c0cd7b86d9f9b580fb3f98fc1415df2
yuichi2670 <yuichitao2670@gmail.com>
2021-11-28 16:44:49 +0900

    project sdgs

--- marine-associate/front-page.php
+++ marine-associate/front-page.php
@@ -105,6 +105,145 @@
     </div><!-- /p-projects__inner -->
   </section><!-- /p-projects -->
 
+  <div class="l-project p-project">
+    <div class="p-project__inner l-inner">
+      <div class="l-project__container p-project__container">
+        <div class="l-project__content p-project__content">
+          <div class="p-project__number">
+            <p>PROJECT<span>01</span></p>
+          </div><!-- /p-project__number -->
+          <div class="p-project__title">
+            <h2>まちづくり</h2>
+          </div><!-- /project__title -->
+        </div><!-- /p-project__content -->
+        <div class="l-project__textBox p-project__textBox">
+          <div class="l-project__icon p-project__icon">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/project-icon01.png" alt="">
+          </div><!-- /p-project__icon -->
+          <div class="p-project__text-short">
+            <p>サンプルテキストサンプルテキ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-middle">
+            <p>サンプルテキストサンプルテキストサンプ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-long">
+            <p>Toward the realization of "what I want to achieve in my lifetime” , We will do our best to support you.</p>
+          </div><!-- /p-project__text -->
+        </div><!-- /p-project__textBox -->
+        <div class="l-project__btn p-project__btn">
+          <a href="">MORE</a>
+        </div><!-- / -->
+      </div><!-- /p-project__container -->
+    </div><!-- /p-project__inner -->
+  </div><!-- /p-project -->
+
+  <div class="l-project p-project">
+    <div class="p-project__inner l-inner">
+      <div class="l-project__container2 p-project__container">
+        <div class="l-project__content2 p-project__content">
+          <div class="p-project__number">
+            <p>PROJECT<span>02</span></p>
+          </div><!-- /p-project__number -->
+          <div class="p-project__title">
+            <h2>ひとづくり</h2>
+          </div><!-- /project__title -->
+        </div><!-- /p-project__content -->
+        <div class="l-project__textBox2 p-project__textBox">
+          <div class="l-project__icon2 p-project__icon">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/project-icon02.png" alt="">
+          </div><!-- /p-project__icon -->
+          <div class="p-project__text-short">
+            <p>サンプルテキストサンプルテキ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-middle">
+            <p>サンプルテキストサンプルテキストサンプ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-long">
+            <p>Toward the realization of "what I want to achieve in my lifetime” , We will do our best to support you.</p>
+          </div><!-- /p-project__text -->
+        </div><!-- /p-project__textBox -->
+        <div class="l-project__btn2 p-project__btn">
+          <a href="">MORE</a>
+        </div><!-- / -->
+      </div><!-- /p-project__container -->
+    </div><!-- /p-project__inner -->
+  </div><!-- /p-project -->
+
+  <div class="l-project p-project">
+    <div class="p-project__inner l-inner">
+      <div class="l-project__container3 p-project__container">
+        <div class="l-project__content p-project__content">
+          <div class="p-project__number">
+            <p>PROJECT<span>03</span></p>
+          </div><!-- /p-project__number -->
+          <div class="p-project__title">
+            <h2>未来づくり</h2>
+          </div><!-- /project__title -->
+        </div><!-- /p-project__content -->
+        <div class="l-project__textBox p-project__textBox">
+          <div class="l-project__icon p-project__icon">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/project-icon03.png" alt="">
+          </div><!-- /p-project__icon -->
+          <div class="p-project__text-short">
+            <p>サンプルテキストサンプルテキ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-middle">
+            <p>サンプルテキストサンプルテキストサンプ</p>
+          </div><!-- /p-project__text -->
+          <div class="p-project__text-long">
+            <p>Toward the realization of "what I want to achieve in my lifetime” , We will do our best to support you.</p>
+          </div><!-- /p-project__text -->
+        </div><!-- /p-project__textBox -->
+        <div class="l-project__btn p-project__btn">
+          <a href="">MORE</a>
+        </div><!-- / -->
+      </div><!-- /p-project__container -->
+    </div><!-- /p-project__inner -->
+  </div><!-- /p-project -->
+
+  <section class="l-access p-access">
+    <div class="p-access__inner l-inner">
+      <div class="p-access__container">
+        <div class="p-access__img">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/access.jpg" alt="">
+        </div><!-- /p-access__img -->
+        <div class="l-access__box p-access__box">
+          <div class="p-access__title">
+            <h2>ACCESS</h2>
+          </div><!-- /p-access__title -->
+          <div class="p-access__subtitle">
+            <h3>全国でも有数の広さを誇る渋川海岸。<br>瀬戸内の島々の絶景も楽しめる最高のスポット。</h3>
+          </div><!-- /p-access__subtitle -->
+          <div class="l-access__text p-access__text">
+            <p>私たちは、渋川・王子が岳エリアの観光振興を推進する団体です。<br>地域発展において、主体性を持った人財は、正に地域の宝と言えます。</p>
+            <p>「よそ者」「わか者」「ばか者」が地域を変えると言いますが、<br>それに加えて、地元の理解と調和が大変重要であると考えております。</p>
+            <p>渋川マリンアクティビティ協会は、この美しい瀬戸内の地域に根付き、<br>地元の課題を踏まえながら、主体性のある人財確保と次世代のリーダー育成に努めて参ります。</p>
+          </div><!-- /p-about__text -->
+          <div>
+            <a href="" class="l-access__btn p-access__btn">Google Map</a>
+          </div><!-- / -->
+        </div><!-- /p-access__box -->
+      </div><!-- /p-access__container -->
+    </div><!-- /p-access__inner -->
+  </section>
+
+  <section class="l-sdgs p-sdgs">
+    <div class="p-sdgs__inner">
+      <div class="p-sdgs__img">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/sdgs.jpg" alt="">
+      </div><!-- /p-sdgs__img -->
+      <div class="p-sdgs__textBox">
+        <div class="p-sdgs__title">
+          <h2>私たちは、SDGsの達成に貢献します。</h2>
+        </div><!-- /p-sdgs__title -->
+        <div class="p-sdgs__border"></div><!-- /p-sdgs__border -->
+        <div class="p-sdgs__text">
+          <p>Sustainable Development Goals<br>2015年、持続可能な世界を実現するために、国連加盟国193カ国全会一致で「持続可能な開発目標（SDGs）」が採択されました。2030年までに世界が達成すべき17の目標と169のターゲットです。「誰一人取り残されない」社会の実現をめざしています。</p>
+        </div><!-- /p-sdgs__text -->
+      </div><!-- /p-sdgs__textBox -->
+    </div><!-- /p-sdgs__inner -->
+  </section><!-- /p-sdgs -->
+
 
   <div class="l-top p-top">
     <a href="#" class="p-top__items">
